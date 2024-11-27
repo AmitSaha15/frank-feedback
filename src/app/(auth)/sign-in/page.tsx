1a941ed7c655330160721d342323dd17cf529c47
Amit Saha <asaha.bwn2000@gmail.com>
2024-11-28 01:40:52 +0530

    mistake pe mistake!

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -109,7 +109,7 @@ const Page = () => {
         </Form>
         <div className="text-center mt-4">
           <p>
-            Don't have an account?{' '}
+            Don't have an account ? {' '}
             <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
               Sign Up
             </Link>
